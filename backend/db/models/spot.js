469af0fcf5bd6b99734d591044ecc4f24b1d7c41
Arko Chakrabarty <c.arko.business@gmail.com>
2022-11-23 09:24:27 -0800

    added all accosiations and references to migrations and models and all initial constraints and validations

---
+++ backend/db/models/spot.js
@@ -0,0 +1,106 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Spot extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Spot.belongsToMany(models.User, {through: 'Booking',
+       foreignKey: 'spotId',
+       otherKey: 'userId'
+    });
+      Spot.hasMany(models.reviews, {foreignKey: 'spotId'})
+    }
+  }
+  Spot.init({
+    ownerId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isNumeric: true
+      }
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [2, Infinity]
+      }
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isAlpha: true,
+        len: [2, Infinity]
+      }
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isAlpha: true,
+        len: [2, Infinity]
+      }
+    },
+    country: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isAlpha: true,
+        len: [2, Infinity]
+      }
+    },
+    lat: {
+      type: DataTypes.REAL,
+      allowNull: false,
+      validate: {
+        isNumeric: true,
+        len: [2, Infinity],
+        min: -90,
+        max: 90
+      }
+    },
+    lng: {
+      type: DataTypes.REAL,
+      allowNull: false,
+      validate: {
+        isNumeric: true,
+        len: [2, Infinity],
+        min: -180,
+        max: 180
+      }
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isAlpha: true,
+        len: [2, Infinity]
+      }
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isNumeric: true,
+        min: 0
+      }
+    },
+  }, {
+    sequelize,
+    modelName: 'Spot',
+  });
+  return Spot;
+};
--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -41,6 +41,11 @@ module.exports = (sequelize, DataTypes) => {
     }
     static associate(models) {
       // define association here
+      User.belongsToMany(models.Spot, {through: 'Booking',
+      foreignKey: 'userId',
+      otherKey: 'spotId'});
+      User.hasMany(models.Spot, {foreignKey: 'ownerId'});
+      User.hasMany(models.Review, {foreignKey: 'userId'})
     }
   };
 
