425e1dc92962cc8f96b543b938909d961d700953
Arko Chakrabarty <c.arko.business@gmail.com>
2022-12-12 01:29:08 -0800

    undo my undo

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,16 +7,21 @@ router.use('/api', apiRouter);
 
 // Static routes
 // Serve React build files in production
+
+// ... after `router.use('/api', apiRouter);`
+
+// Static routes
+// Serve React build files in production
 if (process.env.NODE_ENV === 'production') {
   const path = require('path');
   // Serve the frontend's index.html file at the root route
   router.get('/', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.sendFile(
+    return res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
-
+}
   // Serve the static assets in the frontend's build folder
   router.use(express.static(path.resolve("../frontend/build")));
 
@@ -27,7 +32,7 @@ if (process.env.NODE_ENV === 'production') {
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
-}
+
 
 // Add a XSRF-TOKEN cookie in development
 if (process.env.NODE_ENV !== 'production') {
@@ -36,5 +41,6 @@ if (process.env.NODE_ENV !== 'production') {
     res.status(201).json({});
   });
 }
+
 
 module.exports = router;
